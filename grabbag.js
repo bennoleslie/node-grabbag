4c24ac4e3cccdaf7dd93c2d08fb6205e419e1701
Ben Leslie <benno@benno.id.au>
2011-09-23 11:17:58 -0400

    Fixup truncated line

--- grabbag.js
+++ grabbag.js
@@ -78,7 +78,7 @@ exports.binaryModule = binaryModule
  * See: http://tools.ietf.org/html/rfc6265
  */
 function parseCookie(cookie) {
-    if (cookie === undefi
+    if (cookie === undefined) {
         return {}
     }
 
